e2bf664cea9ffd9022798e0ae23abcdf185be563
Valentin Burillier <valentin.burillier@gmail.com>
2023-03-09 12:36:06 +0100

    start article page

---
+++ html/articles/naissance-du-globe-terrestre.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G0WCYZLV8G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G0WCYZLV8G');
+</script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <meta name="description" content="Selection unique de Globes Terrestres. Voyagez depuis chez vous avec nos modèles anti-gravite, politiques, historiques et bien d'autres encore.">
+  <link rel="icon" type="image/png" href="assets/images/globes-bar.png" />
+  <title>Globes terrestres</title>
+  <meta name="google-site-verification" content="e2nUTaIyamp5zJBpWH08O5CL2snDA6JSNf6OISJbmj0" />
+</head>
+<body>
+  <video autoplay muted loop id="myVideo">
+    <source src="../../Pexels Videos 1851190.mp4" type="video/mp4">
+  </video>
+<!--START END-->
+<header>
+  <h1>Globes terrestres - Compagnie Française</h1>
+</header>
+<!--END HEADER-->
+
+<!--START MAIN-->
+<main>
+  <article>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusamus odit illo illum? Porro accusantium impedit, tempore repudiandae sunt earum assumenda quod minima nesciunt, quae nam facilis, voluptates hic numquam.</p>
+  </article>
+</main>
+
+<!--START FOOTER-->
+<footer>
+<ul>
+  <li>
+    <a href="html/politiques-de-confidentialite.html">Politiques de confidentialité</a>
+  </li>
+  <li>
+    <a href="html/conditions-generales-de-ventes.html">CGV</a>
+  </li>
+  <li>
+    <a href="sitemap.xml">Sitemap</a>
+  </li>
+</ul>
+</footer>
+</body>
+</html>
